364f8638f7c3f7309e349224f2c7be0a31127775
dfherr <deflo.herr@gmail.com>
2017-07-21 15:39:59 +0200

    fixed spec and assets issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_711_122_812) do
+ActiveRecord::Schema.define(version: 20170711122812) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,4 +40,5 @@ ActiveRecord::Schema.define(version: 20_170_711_122_812) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
+
 end
