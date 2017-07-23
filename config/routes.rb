71bf0e5af48ab43797c311690115aca27a5bf46e
dfherr <deflo.herr@gmail.com>
2017-07-23 22:53:32 +0200

    added static footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "home#index"
+  get "/imprint", to: "home#imprint"
 
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
