fc60c06388150bd00eba4d2b35a6e4eb861ac944
dfherr <deflo.herr@gmail.com>
2017-07-26 15:06:38 +0200

    remove overlooked log_level setting from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,10 +33,6 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
-
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
@@ -82,8 +78,8 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    config.log_level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_sym : :warn
   end
+  config.log_level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_sym : :warn
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
