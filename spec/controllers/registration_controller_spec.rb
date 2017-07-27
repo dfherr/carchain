36b763a346c4264f9ae9d80ccd553657b818414b
dfherr <deflo.herr@gmail.com>
2017-07-27 14:37:34 +0200

    file upload spec example

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -4,12 +4,27 @@ RSpec.describe Car::RegistrationController, type: :controller do
   let(:user) {
     create(:confirmed_user)
   }
+  before :each do
+    sign_in user
+  end
 
   describe "GET index" do
     it "renders the car overview" do
-      sign_in user
       get :index
       expect(response).to render_template("index")
     end
   end
+
+  # describe "POST create_registration" do
+  #   before :each do
+  #      @file = fixture_file_upload(Rails.root.join('spec/fixtures/files/pdf-sample.pdf'), 'application/pdf')
+  #   end
+  #   it "renders the car overview" do
+  #     post :create_registration
+  #     expect(response).to redirect_to car_registration_path
+  #   end
+  #   after :each do
+  #     @file.close
+  #   end
+  # end
 end
